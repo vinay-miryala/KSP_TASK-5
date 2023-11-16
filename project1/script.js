ea223a5216547871279b66c0c9c194ffe5166651
Vinay_miryala <149994730+vinay-miryala@users.noreply.github.com>
2023-11-16 13:11:33 +0530

    Add files via upload

---
+++ project1/script.js
@@ -0,0 +1,24 @@
+window.alert("welcome to my webpage : happu to see u all");
+
+let var1 = 3;
+let var2 = 5;
+console.log(var1);
+
+const num3 = 60;
+console.log(num3);
+
+
+ageOfAPerson = 35;
+console.log(ageOfAPerson);
+
+iAmInterestedIn ="singing";
+console.log(iAmInterestedIn);
+
+let num1 = 8;
+let num2 = 4;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(num1 % num2);
+console.log(num1 ** num2)
